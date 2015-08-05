f901efd6a9eb2fd4c6684747656a933c8a7c04b2
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-08-05 15:29:59 +0200

    Initial commit

---
+++ wallaby.js
@@ -0,0 +1,32 @@
+module.exports = function () {
+
+  return {
+    files: [
+      'bin/*.js',
+      'lib/**/*.js',
+      'index.js',
+      '__mocks__/*.js'
+    ],
+
+    tests: [
+      '__tests__/*.js'
+    ],
+
+    env: {
+      type: 'node',
+      runner: 'node',
+      params: {
+        runner: '--harmony'
+      }
+    },
+
+    testFramework: 'jest',
+
+    bootstrap: function (wallaby) {
+      var path = require('path');
+      var packageConfigPath = path.resolve(wallaby.localProjectDir, 'package.json');
+      var packageConfig = require(packageConfigPath);
+      wallaby.testFramework.configure(packageConfig.jest);
+    }
+  };
+};
