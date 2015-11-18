f8403de428f654e9b3cf9a2c28e3d62f1f4a1660
lgandecki <lgandecki@css.edu>
2015-11-18 12:56:25 +0100

    updates for multidriver functionality

--- index.js
+++ index.js
@@ -22,8 +22,14 @@ webdriverioWithSync.wrapAsyncObject = wrapAsyncObject;
 
 webdriverioWithSync.remote = function (options) {
   var syncByDefault = !(options && options.sync === false);
+  var multiremote = options.browser0 ? true : false;
+  var remote;
 
-  var remote = webdriverio.remote.apply(webdriverio, arguments);
+  if (multiremote) {
+    remote = webdriverio.multiremote.apply(webdriverio, arguments);
+  } else {
+    remote = webdriverio.remote.apply(webdriverio, arguments);
+  }
 
   // Run condition function in fiber
   var waitUntil = remote.waitUntil;
@@ -36,42 +42,79 @@ webdriverioWithSync.remote = function (options) {
   // Wrap async all core commands
   var webdriverioPath = path.dirname(require.resolve('webdriverio'));
   var commandNames = _.chain(['protocol', 'commands'])
-    .map(function(commandType) {
-      var dir = path.resolve(webdriverioPath, path.join('lib', commandType));
-      var files = fs.readdirSync(dir);
-      return files.map(function(filename) {
-        return filename.slice(0, -3);
-      });
-    })
-    .flatten(true)
-    .uniq()
-    .value();
+      .map(function(commandType) {
+        var dir = path.resolve(webdriverioPath, path.join('lib', commandType));
+        var files = fs.readdirSync(dir);
+        return files.map(function(filename) {
+          return filename.slice(0, -3);
+        });
+      })
+      .flatten(true)
+      .uniq()
+      .value();
 
-  var remoteWrapper = wrapAsyncObject(remote, commandNames, {
-    syncByDefault: syncByDefault,
-    wrapAsync: wrapAsyncForWebdriver
-  });
+  var _browsers = [];
+  var _wrappers = [];
+  var index = 0;
 
-  // Wrap async added commands
-  var addCommand = remote.addCommand;
-  remote.addCommand = function (fnName, fn, forceOverwrite) {
-    var result = addCommand.call(remote, fnName, Promise.async(fn), forceOverwrite);
-    var commandWrapper = wrapAsyncObject(remote, [fnName], {
+  wrapAsyncBrowser(remote);
+  var remoteWrapper = _wrappers[0];
+
+  if (multiremote) {
+
+    Object.keys(options).forEach(function(browserName) {
+      var browser = remote.select(browserName);
+      wrapAsyncBrowser(browser);
+    });
+
+    remoteWrapper.browsers = [];
+    _wrappers.forEach(function(wrapper, index) {
+      // if one of the multiremote browsers
+      if (index > 0) {
+        remoteWrapper.browsers.push(wrapper);
+      }
+    });
+    remoteWrapper.init();
+  }
+
+
+
+  function wrapAsyncBrowser(browser) {
+    console.log("_browsers.length", _browsers.length);
+    _browsers.push(browser);
+    _browsers[index].index = index;
+    _wrappers.push(wrapAsyncObject(_browsers[index], commandNames, {
       syncByDefault: syncByDefault,
       wrapAsync: wrapAsyncForWebdriver
+    }));
+    console.log("_browsers index after", _browsers.length);
+
+    var _addCommand = {};
+    // Wrap async added commands
+    _browsers[index]._addCommand = _browsers[index].addCommand;
+    _browsers[index].addCommand = function(fnName, fn, forceOverwrite) {
+      console.log("index in addCommand ", this.index);
+      var result = _browsers[this.index]._addCommand.call(_browsers[index], fnName, Promise.async(fn), forceOverwrite);
+      var commandWrapper = wrapAsyncObject(_browsers[this.index], [fnName], {
+        syncByDefault: syncByDefault,
+        wrapAsync: wrapAsyncForWebdriver
+      });
+      _.defaults(_wrappers[this.index], commandWrapper);
+      return result;
+    };
+
+    _.forEach([
+      'addCommand',
+      'transferPromiseness',
+      'on', 'once', 'emit', 'removeListener', 'removeAllListeners', 'select'
+    ], function(methodName) {
+      if (_browsers[index][methodName]) {
+        _wrappers[index][methodName] =
+            _browsers[index][methodName].bind(_browsers[index]);
+      }
     });
-    _.defaults(remoteWrapper, commandWrapper);
-
-    return result;
-  };
-
-  _.forEach([
-    'addCommand',
-    'transferPromiseness',
-    'on', 'once', 'emit', 'removeListener', 'removeAllListeners'
-  ], function (methodName) {
-    remoteWrapper[methodName] = remote[methodName].bind(remote);
-  });
+    index++;
+  }
 
   return remoteWrapper;
 };
