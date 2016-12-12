ad3eb1af1fbbc21ff63861d8bcc5ab4997c48956
lucetius <lucetius@lucetius.pl>
2016-12-12 18:29:29 +0100

    Upgrade wdio-sync to v0.6.6 (#5)
    * Upgrade wdio-sync to v0.6.6
    * Fix global.browser.option when not set

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ var wrapCommand = require('wdio-sync').wrapCommand;
 var wrapAsyncObject = require('xolvio-fiber-utils').wrapAsyncObject;
 
 var wrapAsyncForWebdriver = function (fn, context) {
+  if (!global.browser) {
+    global.browser = { options: { sync: true } };
+  }
+  else if (!global.browser.options) {
+    global.browser.options = { sync: true };
+  }
   return wrapCommand(fn.bind(context), fn.name, _.noop, _.noop);
 };
 
